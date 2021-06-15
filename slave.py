a533b017eeddb552f31f1a4411473892f30cd5f1
daniff15 <dani.fig@ua.pt>
2021-06-15 12:06:05 +0100

    httpbasicauth

--- slave.py
+++ slave.py
@@ -1,4 +1,14 @@
 import time
+from http.client import HTTPSConnection
+from base64 import b64encode
+import requests
+from requests.auth import HTTPBasicAuth
+
+response = requests.get('http://0.0.0.0:8000',
+auth = HTTPBasicAuth('root', 'L'))
+# COM ISTO JA CONSEGUE CORRER O SLAVE E ENONTRAR A PASS, NO SEGUNDO ARG DE HTTPBASICAUTH
+
+print(response)
 
 while True:
     print("all your base are belong to us")
